84e2f68ab252a1a58d53b4778dc73b6b04441bf3
Peter Grubelnik <grubelnik.peter@gmail.com>
2025-03-03 20:22:49 +0100

    feat(ralff): Enable podman

--- ralff-fe/vite.config.ts
+++ ralff-fe/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3055
+    host: '0.0.0.0',
+    port: 3055,
+    strictPort: false
   }
 })
